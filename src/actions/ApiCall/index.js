68903473d1df01a081ed7b49d3fcbf724bf77416
datchanhkun <caothanhdat.org@gmail.com>
2021-09-05 02:31:32 +0700

    drag drop fix call multiple api

--- src/actions/ApiCall/index.js
+++ src/actions/ApiCall/index.js
@@ -1,6 +1,12 @@
 import axios from 'axios'
 import { API_ROOT } from 'ultilities/constants'
 
+//Call API Update Board
+export const updateBoard = async (id, data) => {
+  const request = await axios.put(`${API_ROOT}/v1/boards/${id}`, data)
+  return request.data
+}
+
 //Call API Get Full Board
 export const fetchBoardDetails = async (id) => {
   const request = await axios.get(`${API_ROOT}/v1/boards/${id}`)
@@ -22,5 +28,11 @@ export const updateColumn = async (id, data) => {
 //Call API Create New Card
 export const createNewCard = async (data) => {
   const request = await axios.post(`${API_ROOT}/v1/cards`, data)
+  return request.data
+}
+
+//Call API Create New Card
+export const updateCard = async (id, data) => {
+  const request = await axios.put(`${API_ROOT}/v1/cards/${id}`, data)
   return request.data
 }
